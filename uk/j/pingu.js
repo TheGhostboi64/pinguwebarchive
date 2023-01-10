5425acebd57f0bd55f0ceec2d54a9ea9f7cf6951
TheGhostboi64 <99986496+TheGhostboi64@users.noreply.github.com>
2023-01-09 20:04:14 -0500

    Create pingu.js

---
+++ uk/j/pingu.js
@@ -0,0 +1,23 @@
+function addLoadEvent(func) {
+  var oldonload = window.onload;
+  if (typeof window.onload != 'function') {
+    window.onload = func;
+  } else {
+    window.onload = function() {
+      oldonload();
+      func();
+    }
+  }
+}
+
+function addClass(element,value) {
+  if (!element.className) {
+    element.className = value;
+  } else {
+    newClassName = element.className;
+    newClassName+= " ";
+    newClassName+= value;
+    element.className = newClassName;
+  }
+}
+var flash_uri = document.getElementById("flash_uri").value
