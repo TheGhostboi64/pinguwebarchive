47e08e9dddfca28921d2fb7bb1aec57e7b6c5e69
TheGhostboi64 <99986496+TheGhostboi64@users.noreply.github.com>
2023-01-10 19:10:10 -0500

    Create pingu.js

--- uk/j/pingu.js
+++ uk/j/pingu.js
@@ -1,3 +1,15 @@
+function addLoadEvent(func) {
+  var oldonload = window.onload;
+  if (typeof window.onload != 'function') {
+    window.onload = func;
+  } else {
+    window.onload = function() {
+      oldonload();
+      func();
+    }
+  }
+}
+
 function addClass(element,value) {
   if (!element.className) {
     element.className = value;
